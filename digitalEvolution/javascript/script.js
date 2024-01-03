ee880cdb94fc420dbbb008c55becaf1d865228ac
EugenTescan <116834632+EugenTescan@users.noreply.github.com>
2024-01-03 13:25:00 +0200

    Delete digitalEvolution directory

--- digitalEvolution/javascript/script.js
+++
@@ -1,9 +0,0 @@
-let menu = document.querySelector(".menu-section");
-
-document.querySelector("#menu-icon").onclick=() => {
-    menu.classList.toggle('active');
-}
-
-window.onscroll = () => {
-    menu.classList.remove('active');
-}
